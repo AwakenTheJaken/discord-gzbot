ee1939ad3d07cba7f220550d5ee54a08546beaff
JAKEN <jake@devpotion.com>
2021-07-25 10:54:14 -0700

    added more body parts to the list

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ client.on("message", function (message) {
   const command = args.shift().toLowerCase();
 
   if (command === 'rubbot') {
-    const verbs = ['Ineptly','Incompetently','Weakly','Poorly','Frantically','Disinterestedly', 'Ineffectively', 'Incorrectly', 'Mechanically','Compassionately', 'Calmly', 'Gently', 'Nonchalantly', 'Mildly', 'Politely', 'Provacativley']
+    const verbs = ['Ineptly','Incompetently','Weakly','Poorly','Frantically','Disinterestedly', 'Ineffectively', 'Incorrectly', 'Mechanically','Compassionately', 'Calmly', 'Gently', 'Nonchalantly', 'Mildly', 'Politely', 'Provocatively']
     const randomVerb = verbs[Math.floor(Math.random() * verbs.length)]
     const actions = ['juggles', 'pushes','pulls','tenderises','pinches','pokes','punches','rubs', 'strokes', 'massages', 'caresses', 'pats']
     const randomAction = actions[Math.floor(Math.random() * actions.length)]
-    const bodyParts = ['calves','knee','temporal lobe','weenis','calcaneus','sacrum','iliac crest','second metatarsal','humerus','eyes', 'nose', 'funny bone','fingernails', 'femur', 'septum', 'spleen','pancreas', 'elbow', 'big toe']
+    const bodyParts = ['spine','ribs','liver','hair','ear','calves','knee','temporal lobe','weenis','calcaneus','sacrum','iliac crest','second metatarsal','humerus','eyes', 'nose', 'funny bone','fingernails', 'femur', 'septum', 'spleen','pancreas', 'elbow', 'big toe']
     const randomBodyPart = bodyParts[Math.floor(Math.random() * bodyParts.length)]
     if(message.mentions.members.first()){
       var targetUser =  message.mentions.members.first().user
