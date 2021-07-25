a50e8c40f1b86e514661b52ec15a0325f0322885
JAKEN <jake@devpotion.com>
2021-07-25 09:52:03 -0700

    Hotfix: logging

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ client.on("message", function (message) {
       var targetUser =  message.mentions.members.first().user
     }else{
       var targetUser = message.channel.guild.members.cache.filter(member => member.presence.status !== 'offline' && member.user.id !== message.member.id && member.roles.cache.some(role => role.name !== 'botsquad' || role.name !== 'Botsquad') || member.name != 'GZBOT').random()
+      console.log(message.channel.guild.members.cache.filter(member => member.presence.status !== 'offline' && member.user.id !== message.member.id && member.roles.cache.some(role => role.name !== 'botsquad' || role.name !== 'Botsquad') || member.name != 'GZBOT'))
     }
     client.channels.cache.get(message.channel.id).send(`Passes ${targetUser} ${randomCrayon} ${randomCrayonColor.toLowerCase()} colored ${crayon}.`);
 
