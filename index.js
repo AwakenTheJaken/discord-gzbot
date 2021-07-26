435ff74120bdff701939cff68e13c1a0f3c3de22
JAKEN <jake@devpotion.com>
2021-07-26 14:57:08 -0700

    Feature: added 8ball

--- index.js
+++ index.js
@@ -31,7 +31,12 @@ client.on("message", function (message) {
   if (command === '8ball') {
     const predictions = ["It is certain", "Without a doubt","You may rely on it","Yes definitely","It is decidedly so","As I see it, yes","Most likely","Yes","Outlook good","Signs point to yes","Reply hazy try again","Better not tell you now","Ask again later","Cannot predict now","Concentrate and ask again","Don’t count on it","Outlook not so good","My sources say no","Very doubtful","My reply is no"]
     const randomPrediction = predictions[Math.floor(Math.random() * predictions.length)]
-    client.channels.cache.get(message.channel.id).send(`${randomPrediction}`);
+    const userQuestion = message.content.split('!8ball')
+    if(userQuestion.length){
+      client.channels.cache.get(message.channel.id).send(`${randomPrediction}`);
+    }else{
+      client.channels.cache.get(message.channel.id).send(`Please ask a question!`);
+    }
   }
 
   if (command === 'ama') {
