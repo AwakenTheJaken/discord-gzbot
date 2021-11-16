823e0849b291c6a8ead394de59ff5ac58229df7c
JAKEN <jake@devpotion.com>
2021-11-15 16:22:26 -0800

    Feature: added superfight!

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+let superfight = require('./superfight.json');
+
+
 client.on('ready', () => {
     console.info(`Logged in as ${client.user.tag}!`);
   });
@@ -60,7 +63,31 @@ client.on("message", function (message) {
         client.channels.cache.find(channel => channel.name === '🎤-ama-questions').send(questionAskedConfirmation);
       }
     }
-  
+    
+    if(command === 'superfight'){
+      var messageContent = message.content.split('!superfight ')[1]
+
+      console.log(messageContent)
+
+      if(messageContent === 'roll'){
+        var character = superfight['characters'][Math.floor(Math.random() * superfight['characters'].length)]
+        var attributes = randomSuperfightAttribute()
+        message.reply(`character: ${character} | attribute 1: ${attributes[0]} | attribute 2: ${attributes[1]}`)
+      }
+      if(messageContent === 'start'){
+        client.channels.cache.get(message.channel.id).send(`------------------------------------------------------------------------------------------------------------------`);
+        var challenge = superfight['challenges'][Math.floor(Math.random() * superfight['challenges'].length)]
+        var scenario = superfight['scenarios'][Math.floor(Math.random() * superfight['scenarios'].length)]
+        var location = superfight['locations'][Math.floor(Math.random() * superfight['locations'].length)]
+        client.channels.cache.get(message.channel.id).send(`location: ${location} | scenario: ${scenario} | challenge: ${challenge}`)
+      }
+      if(messageContent.split('win ')[1]){
+        const winner = messageContent.split('win ')[1]
+        client.channels.cache.get(message.channel.id).send(`${winner} is declared the winner!`)
+        client.channels.cache.get(message.channel.id).send(`------------------------------------------------------------------------------------------------------------------`);
+      }
+    }
+
     if (command === 'crayon') {
       const crayonColors = ["Mahogany", "Fuzzy Wuzzy Brown", "Chestnut", "Red Orange", "Sunset Orange", "Bittersweet", "Melon", "Outrageous Orange", "Vivid Tangerine", "Burnt Sienna", "Brown", "Sepia", "Orange", "Burnt Orange", "Copper", "Mango Tango", "Atomic Tangerine", "Beaver", "Antique Brass", "Desert Sand", "Raw Sienna", "Tumbleweed", "Tan", "Peach", "Macaroni and Cheese", "Apricot", "Neon Carrot", "Almond", "Yellow Orange", "Gold", "Shadow", "Banana Mania", "Sunglow", "Goldenrod", "Dandelion", "Yellow", "Green Yellow", "Spring Green", "Olive Green", "Laser Lemon", "Unmellow Yellow", "Canary", "Yellow Green", "Inch Worm", "Asparagus", "Granny Smith Apple", "Electric Lime", "Screamin Green", "Fern", "Forest Green", "Sea Green", "Green", "Mountain Meadow", "Shamrock", "Jungle Green", "Caribbean Green", "Tropical Rain Forest", "Pine Green", "Robin Egg Blue", "Aquamarine", "Turquoise Blue", "Sky Blue", "Outer Space", "Blue Green", "Pacific Blue", "Cerulean", "Cornflower", "Midnight Blue", "Navy Blue", "Denim", "Blue", "Periwinkle", "Cadet Blue", "Indigo", "Wild Blue Yonder", "Manatee", "Blue Bell", "Blue Violet", "Purple Heart", "Royal Purple", "Purple Mountains’ Majesty", "Violet (Purple)", "Wisteria", "Vivid Violet", "Fuchsia", "Shocking Pink", "Pink Flamingo", "Plum", "Hot Magenta", "Purple Pizzazz", "Razzle Dazzle Rose", "Orchid", "Red Violet", "Eggplant", "Cerise", "Wild Strawberry", "Magenta", "Lavender", "Cotton Candy", "Violet Red", "Carnation Pink", "Razzmatazz", "Piggy Pink", "Jazzberry Jam", "Blush", "Tickle Me Pink", "Pink Sherbet", "Maroon", "Red", "Radical Red", "Mauvelous", "Wild Watermelon", "Scarlet", "Salmon", "Brick Red", "White", "Timberwolf", "Silver", "Gray", "Black"]
       const randomCrayonColor = crayonColors[Math.floor(Math.random() * crayonColors.length)]
@@ -84,11 +111,19 @@ client.on("message", function (message) {
         var targetUser =  message.mentions.members.first().user
       }else{
         var targetUser = message.channel.guild.members.cache.filter(member => member.presence.status !== 'offline' && member.user.id !== message.member.user.id && member.roles.cache.some(role => role.name !== 'botsquad' || role.name !== 'Botsquad') || member.user.username != 'GZBOT').random()
-        console.log( message.channel.guild.members.cache.filter(member => member.presence.status !== 'offline' && member.user.id !== message.member.user.id && member.roles.cache.some(role => role.name !== 'botsquad' || role.name !== 'Botsquad') || member.user.username != 'GZBOT'))
       }
       client.channels.cache.get(message.channel.id).send(`Passes ${targetUser} ${randomCrayon} ${randomCrayonColor.toLowerCase()} ${crayon}.`);
   
     }
 
 });
+function randomSuperfightAttribute(){
+  var attribute0 = superfight['attributes'][Math.floor(Math.random() * superfight['attributes'].length)]
+  var attribute1 = superfight['attributes'][Math.floor(Math.random() * superfight['attributes'].length)]
+  if(attribute0 === attribute1){
+    randomSuperfightAttribute()
+  }else{
+    return [attribute0, attribute1]
+  }
+}
 client.login(process.env.TOKEN)
